26bb7b7e26f25bb3a71b89f7d6551c2b8cca3bb8
mhago <agmohamed1@gmail.com>
2020-10-31 22:23:13 +0000

    BS by iteration

--- Section2/Section2-1.go
+++ Section2/Section2-1.go
@@ -15,15 +15,31 @@ func bsOccurrence(array []int, target int, lowindex int, highindex int) int {
 	}
 }
 
+func bsItertion(array []int, target int, lowindex int, highindex int) int {
+	mid := int((lowindex + highindex) / 2)
+	for lowindex < highindex {
+		mid = int((lowindex + highindex) / 2)
+		if array[mid] > target {
+			highindex = mid
+		} else if array[mid] < target {
+			lowindex = mid
+		} else {
+			return mid
+		}
+	}
+	return -1
+}
+
 func main() {
 	primes := []int{2, 3, 5, 7, 11, 13}
-	target := 22
+	target := 7
 	minindex := 0
 	maxindex := len(primes) - 1
 	if target > primes[maxindex] || target < primes[minindex] {
 		fmt.Println("the target is outside of the array")
 	} else {
-		fmt.Printf("Eureka !!, the target was foud at index : %d", bsOccurrence(primes, target, minindex, maxindex))
+		fmt.Printf("Eureka Ocuurence!!, the target was foud at index : %d \n", bsOccurrence(primes, target, minindex, maxindex))
+		fmt.Printf("Eureka Iteration!!, the target was foud at index : %d", bsItertion(primes, target, minindex, maxindex))
 	}
 
 }
